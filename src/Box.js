698b2a6e98f5b693eca813f758d7b8c8ee01e991
cdsalty <soltiscd@gmail.com>
2019-06-01 16:06:51 -0400

    completed application

--- src/Box.js
+++ src/Box.js
@@ -3,12 +3,14 @@ import { choice } from './helpers';
 import './Box.css';
 
 class Box extends Component {
-  static defaultProps = {
-    allColors: ["purple", "red", "green", "blue", "orange", "magenta", "pink", "violet", "yellow", "orchard"]
-  };
+  // static defaultProps = {
+  //   allColors: ["purple", "red", "green", "blue", "orange", "magenta", "pink", "violet", "yellow", "orchard"]
+  // };
+  // To clean up the code, moved props to other props listed in BoxContainer and then called on it within here, "colors".
   constructor(props){
     super(props);
-    this.state = { color: choice(this.props.allColors) };
+    // this.state = { color: choice(this.props.allColors) }; changed from allColors to colors (fron BoxContainer.js)
+    this.state = { color: choice(this.props.colors) };
     this.handleClick = this.handleClick.bind(this);
   }
   
@@ -16,9 +18,8 @@ class Box extends Component {
   pickColor(){
     let newColor;
       do {
-        newColor = choice(this.props.allColors);  // This event will repeat over and over 
+        newColor = choice(this.props.colors);  // This event will repeat over and over 
       } while (newColor === this.state.color);      // while color is already equal to newColor
-
       this.setState({ color: newColor });
   }
 
@@ -28,7 +29,7 @@ class Box extends Component {
   render(){
     return(
       <div className="Box" style={{backgroundColor: this.state.color}} onClick={this.handleClick}></div>
-    )
+    );
   }
 }
 
--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import Box from './Box';
+import './BoxContainer.css';
 
 class BoxContainer extends Component {
   static defaultProps = {
-    numOfBoxes: 18
+    numOfBoxes: 16,  
+    allColors: ["purple", "red", "green", "blue", "orange", "magenta", "pink", "violet", "yellow", "orchard"]
   };
 
   render(){ // inside render, need to loop through a certain amount of times and create 'x' boxes. (determined by defaultProps)
     const boxes = Array.from({ length: this.props.numOfBoxes }).map(() => (
-                // Create an array from zero to the length of numOfBoxes. Then map through each item in array
-                // and for each element, we will return a box. Over the course of mapping through all the props
-                // the total will be 18 boxes created via the map funciton.
-      <Box />
+      <Box colors={this.props.allColors} />
     ));
     return(
       <div className = 'BoxContainer'>   
@@ -22,3 +21,6 @@ class BoxContainer extends Component {
 }
 
 export default BoxContainer;
+              // Create an array from zero to the length of numOfBoxes. Then map through each item in array
+                // and for each element, we will return a box. Over the course of mapping through all the props
+                // the total will be 18 boxes created via the map funciton.
